f873928c7724badd4878232ffdb7f6f5ed7974a6
lungavazi <39201262+lungavazi@users.noreply.github.com>
2021-06-22 17:48:52 +0200

    Update Startup.cs

--- ToDoApi/Startup.cs
+++ ToDoApi/Startup.cs
@@ -42,12 +42,12 @@ namespace ToDoApi
                 o.UseSqlServer(connectionString);
             });
 
-            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            //.AddJwtBearer(options =>
-            //{
-            //    options.Authority = "{https://localhost:44337}";
-            //    //options.Audience = "{}";
-            //});
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "{https://localhost:44337}";
+                //options.Audience = "{}";
+            });
 
             services.AddAuthentication(options =>
             {
@@ -70,7 +70,6 @@ namespace ToDoApi
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
